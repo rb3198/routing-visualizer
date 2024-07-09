168cd11d2946c250804d9473b69ae4f4b8b41c4d
rb3198 <ronitbhatia@hotmail.com>
2024-07-09 00:20:46 -0400

    1: Setup Basic Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+interface HeaderProps {}
+
+export const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <header id="header">
+      <h2 id="header-title">Routing Visualizer</h2>
+      <nav>
+        <ul>
+          {/* Open a modal showing a step by step tutorial */}
+          <li>Tutorial</li>
+          {/* Open a modal showing a list of presets */}
+          <li>Presets</li>
+          {/* Open a modal giving the option to load a json file describing the setup */}
+          <li>Load a Config</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
