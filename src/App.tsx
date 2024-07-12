2fee1e56d0da6430f48d76aeec0260c302031201
rb3198 <ronitbhatia@hotmail.com>
2024-07-12 14:41:00 -0400

    2: Setup Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Grid } from "./components/grid";
 import { Header } from "./components/header";
+import { ComponentPicker } from "./components/network_component_picker";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ComponentPicker />
+      <Grid />
     </div>
   );
 }
