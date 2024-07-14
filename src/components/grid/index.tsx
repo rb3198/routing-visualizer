3e1d3706fff415e8f301ba8b27978000cbb6d8eb
rb3198 <ronitbhatia@hotmail.com>
2024-07-14 12:35:08 -0400

    5: Open Picker on grid cell click

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,7 +1,17 @@
-import React, { useLayoutEffect, useRef, useState } from "react";
+import React, {
+  MouseEventHandler,
+  useCallback,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import styles from "./style.module.css";
 import { debounce } from "../../utils/ui";
 import { Grid as GridEntity } from "../../entities/Grid";
+import { CiRouter } from "react-icons/ci";
+import { PiRectangleDashed } from "react-icons/pi";
+import { ComponentPicker, PickerOption } from "../picker";
 interface GridProps {
   gridSize: number;
 }
@@ -9,18 +19,44 @@ interface GridProps {
 export const Grid: React.FC<GridProps> = (props) => {
   const { gridSize } = props;
   const containerRef = useRef<HTMLCanvasElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
   const gridRef = useRef<GridEntity>();
+  const [tooltip, setTooltip] = useState({
+    visible: false,
+    top: -200,
+    left: -200,
+  });
+
+  const pickerOptions: PickerOption[] = useMemo(
+    () => [
+      {
+        label: "Router",
+        Icon: CiRouter,
+        onClick: () => {},
+      },
+      {
+        label: "AS Boundary",
+        Icon: PiRectangleDashed,
+        onClick: () => {},
+      },
+    ],
+    []
+  );
   useLayoutEffect(() => {
     const onResize = debounce(() => {
       const { documentElement } = document;
       const { clientHeight, clientWidth } = documentElement;
       if (containerRef.current) {
-        containerRef.current.height = 0.8 * clientHeight;
+        containerRef.current.height = 0.92 * clientHeight;
         containerRef.current.width = clientWidth;
         if (!gridRef.current) {
           gridRef.current = new GridEntity(gridSize, containerRef);
         }
         gridRef.current.drawGrid();
+        containerRef.current.addEventListener(
+          "mousemove",
+          gridRef.current.onMouseOverGrid
+        );
       }
     }, 100);
     if (containerRef.current) {
@@ -29,12 +65,74 @@ export const Grid: React.FC<GridProps> = (props) => {
     }
     return () => {
       window && window.removeEventListener("resize", onResize);
+      if (containerRef.current) {
+        containerRef.current.removeEventListener(
+          "mousemove",
+          gridRef.current?.onMouseOverGrid!
+        );
+      }
     };
   }, [gridSize]);
 
+  const { visible, top, left } = tooltip;
+
+  const openTooltip = useCallback((left: number, top: number) => {
+    setTooltip({ visible: true, left, top });
+    if (containerRef.current && gridRef.current) {
+      containerRef.current.removeEventListener(
+        "mousemove",
+        gridRef.current.onMouseOverGrid
+      );
+    }
+  }, []);
+
+  const closeTooltip = useCallback((e: MouseEvent) => {
+    setTooltip({ visible: false, left: -200, top: -200 });
+    if (containerRef.current && gridRef.current) {
+      containerRef.current.addEventListener(
+        "mousemove",
+        gridRef.current.onMouseOverGrid
+      );
+      gridRef.current.onMouseOverGrid(e);
+    }
+  }, []);
+
+  const onCanvasMouseDown: MouseEventHandler<HTMLCanvasElement> = useCallback(
+    (e) => {
+      if (!containerRef.current || !gridRef.current) {
+        return;
+      }
+      gridRef.current.onMouseDown(
+        // @ts-ignore Reason: e incompatible but compatible
+        e,
+        visible,
+        tooltipRef.current,
+        openTooltip,
+        closeTooltip
+      );
+    },
+    [visible, openTooltip, closeTooltip]
+  );
   if (!document) {
     return null;
   }
 
-  return <canvas id={styles.container} ref={containerRef}></canvas>;
+  return (
+    <>
+      <canvas
+        id={styles.container}
+        ref={containerRef}
+        onMouseDown={onCanvasMouseDown}
+      ></canvas>
+      <ComponentPicker
+        pickerRef={tooltipRef}
+        options={pickerOptions}
+        position={{
+          top,
+          left,
+        }}
+        visible={visible}
+      />
+    </>
+  );
 };
--- src/entities/Grid.ts
+++ src/entities/Grid.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, RefObject } from "react";
+import { RefObject } from "react";
 import { Rect } from "./Rect";
 
 export class Grid {
@@ -17,11 +17,28 @@ export class Grid {
    */
   gridRect: Rect[][];
 
+  /**
+   * A set of all locations of the router, stored as `<x coordinate>_<y_coordinate>`
+   */
+  routerLocations: Set<string> = new Set();
+
+  /**
+   * Keeps track of the previous grid location hovered. Helps in clearing the said location when mouse moves over to a different cell.
+   */
+  previousHoverLocation?: [number, number];
+
+  /**
+   * Keeps track of the grid location currently active - picker is in an open state as a result of a click on this location.
+   */
+  activeLocation?: [number, number];
+
   constructor(gridSize: number, canvasRef: RefObject<HTMLCanvasElement>) {
     this.canvasRef = canvasRef;
     this.gridSize = gridSize;
     this.gridRect = this.getEmptyGridRect(gridSize);
   }
+
+  private getLocationKey = (row: number, column: number) => `${row}_${column}`;
 
   private getEmptyGridRect = (gridSize: number) => {
     return new Array(gridSize)
@@ -36,6 +53,126 @@ export class Grid {
     const { width: containerWidth } =
       this.canvasRef.current.getBoundingClientRect();
     return containerWidth / this.gridSize;
+  };
+
+  private mapCoordsToGridCell = (x: number, y: number) => {
+    const cellSize = this.getCellSize();
+    const column = Math.floor(x / cellSize);
+    const row = Math.floor(y / cellSize);
+    return { row, column, rect: this.gridRect[row][column] };
+  };
+
+  /**
+   * Function to handle mouse hovering over the grid.
+   * @param e The Mouse event
+   */
+  onMouseOverGrid = (e: MouseEvent) => {
+    if (!this.canvasRef.current) {
+      return;
+    }
+    const { clientX, clientY } = e;
+    const {
+      x: canvasX,
+      y: canvasY,
+      width: canvasWidth,
+      height: canvasHeight,
+    } = this.canvasRef.current.getBoundingClientRect();
+    const offsetX = clientX - canvasX;
+    const offsetY = clientY - canvasY;
+    if (
+      offsetX < 0 ||
+      offsetX > canvasWidth ||
+      offsetY < 0 ||
+      offsetY > canvasHeight
+    ) {
+      return;
+    }
+    const {
+      row,
+      column,
+      rect: cell,
+    } = this.mapCoordsToGridCell(offsetX, offsetY);
+    const context = this.canvasRef.current.getContext("2d");
+    if (context) {
+      context.fillStyle = "white";
+      if (this.previousHoverLocation) {
+        const [prevRow, prevCol] = this.previousHoverLocation;
+        if (prevRow !== row || prevCol !== column) {
+          this.gridRect[prevRow][prevCol].draw(context);
+        }
+      }
+      cell.drawAddIcon(context);
+      context.fillStyle = "transparent";
+      this.previousHoverLocation = [row, column];
+    }
+  };
+
+  private calcPickerPosition = (
+    row: number,
+    column: number,
+    cell: Rect,
+    tooltipElement: HTMLDivElement
+  ) => {
+    if (!this.canvasRef.current) {
+      return { left: 0, top: 0 };
+    }
+    const { height, width } = tooltipElement.getBoundingClientRect();
+    const { x, y } = cell;
+    const canvasY = this.canvasRef.current.getBoundingClientRect().y;
+    const horizontalPosition =
+      x + width > this.canvasRef.current.clientWidth ? "left" : "right";
+    const verticalPosition =
+      y + height > this.canvasRef.current.clientHeight ? "top" : "bottom";
+    if (horizontalPosition === "right") {
+      if (verticalPosition === "bottom") {
+        const { x, y } = this.gridRect[row + 1][column + 1];
+        return { left: x, top: y + canvasY };
+      }
+      const { x } = this.gridRect[row - 1][column + 1];
+      return { left: x, top: y - height + canvasY };
+    }
+    if (verticalPosition === "bottom") {
+      const { y } = this.gridRect[row + 1][column - 1];
+      return { left: x - width, top: y + canvasY };
+    }
+    return { left: x - width, top: y - height + canvasY };
+  };
+
+  /**
+   * Handle mouse clicks on the grid
+   */
+  onMouseDown = (
+    e: MouseEvent,
+    tooltipOpen: boolean,
+    tooltipElement: HTMLDivElement,
+    openTooltip: (left: number, top: number) => void,
+    closeTooltip: (e?: MouseEvent) => void
+  ) => {
+    const { clientX, clientY } = e;
+    if (!this.canvasRef.current) {
+      return;
+    }
+    const { x: canvasX, y: canvasY } =
+      this.canvasRef.current.getBoundingClientRect();
+    const offsetX = clientX - canvasX,
+      offsetY = clientY - canvasY;
+    const { row, column, rect } = this.mapCoordsToGridCell(offsetX, offsetY);
+    if (tooltipOpen) {
+      closeTooltip(e);
+      return;
+    }
+    const locationKey = this.getLocationKey(row, column);
+    if (!this.routerLocations.has(locationKey)) {
+      // TODO: Add AND !(Paths.contains location key)
+      const { left, top } = this.calcPickerPosition(
+        row,
+        column,
+        rect,
+        tooltipElement
+      );
+      openTooltip(left, top);
+      this.activeLocation = [row, column];
+    }
   };
 
   /**
@@ -56,7 +193,7 @@ export class Grid {
     }
     context.clearRect(0, 0, containerWidth, containerHeight);
     context.fillStyle = "transparent";
-    context.strokeStyle = strokeColor || "grey";
+    context.strokeStyle = strokeColor || "#ddd";
     let y = 0;
     for (let row = 0; row < this.gridSize; row++) {
       let x = 0;
