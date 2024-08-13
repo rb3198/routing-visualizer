c551ba9bedd740a00e89255e5733f30153d58f9f
rb3198 <ronitbhatia@hotmail.com>
2024-08-13 12:23:22 -0400

    2: OneWayReceived Event Handler

--- src/entities/router/ospf_interface/neighbor_event_handlers.ts
+++ src/entities/router/ospf_interface/neighbor_event_handlers.ts
@@ -3,14 +3,19 @@ import { State } from "../../ospf/enum";
 import { NeighborTableRow } from "../../ospf/tables";
 import { deadTimerFactory } from "./timer_factories";
 
+type NeighborEventHandler = (
+  this: OSPFInterface,
+  neighbor: NeighborTableRow
+) => void;
+
 /**
  * The `HelloReceived` Event handler.
  * - Transitions the state of the OSPF Neighbor to `INIT` if it was `DOWN` previously.
  * - Resets the dead timer irrespective of the previous state of the neighbor.
  * @param this The OSPF Interface
- * @param neighbor
+ * @param neighbor The OSPF Neighbor
  */
-export function helloReceived(this: OSPFInterface, neighbor: NeighborTableRow) {
+export const helloReceived: NeighborEventHandler = function (this, neighbor) {
   const { config, neighborTable } = this;
   const { deadInterval } = config;
   const { state, routerId } = neighbor;
@@ -30,4 +35,21 @@ export function helloReceived(this: OSPFInterface, neighbor: NeighborTableRow) {
       state: State.Init,
     });
   }
-}
+};
+
+/**
+ * The `OneWayReceived` event handler. Triggered when a router doesn't see its address in the hello packet it received.
+ * - If the neighbor is in a state >= `2WAY`, regresses the state down to `INIT`
+ * @param this The OSPF Interface
+ * @param neighbor  The OSPF Neighbor
+ */
+export const oneWayReceived: NeighborEventHandler = function (this, neighbor) {
+  const { neighborTable } = this;
+  const { routerId, state } = neighbor;
+  if (state >= State.TwoWay) {
+    neighborTable.set(routerId.toString(), {
+      ...neighbor,
+      state: State.Init,
+    });
+  }
+};
