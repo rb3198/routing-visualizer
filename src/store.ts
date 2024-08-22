97b87289ecf927457f53921b87a8e0779f265a17
rb3198 <ronitbhatia@hotmail.com>
2024-08-22 14:25:49 -0400

    Add extension middleware to the store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,10 @@
-import { applyMiddleware, createStore } from "redux";
+import { createStore } from "redux";
 import { rootReducer } from "./reducers";
-import { thunk } from "redux-thunk";
 
-// @ts-ignore
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  "__REDUX_DEVTOOLS_EXTENSION__" in window &&
+    typeof window["__REDUX_DEVTOOLS_EXTENSION__"] === "function"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : undefined
+);
