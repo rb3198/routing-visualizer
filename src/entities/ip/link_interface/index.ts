79ff129df7a9af6a4ce11214e5fbf84d07487648
rb3198 <ronitbhatia@hotmail.com>
2024-08-22 18:30:54 -0400

    5: Event Based Animations: Decouple packet animations & IP

--- src/entities/ip/link_interface/index.ts
+++ src/entities/ip/link_interface/index.ts
@@ -10,21 +10,7 @@ import {
   getLinkInterfaceCoords,
   getSlopeAngleDist2D,
 } from "../../../utils/drawing";
-import { Point2D } from "../../../types/geometry";
-import { packetAnimationUtils } from "./utils";
-import { getAllRectPointsFromCentroid } from "../../../utils/geometry";
 
-const { getCheckpointCoords, getEndpointCoords, drawPacket } =
-  packetAnimationUtils;
-
-const nextPacketStatus = new Map<
-  "start" | "cp1" | "cp2" | "end",
-  "start" | "cp1" | "cp2" | "end"
->([
-  ["start", "cp1"],
-  ["cp1", "cp2"],
-  ["cp2", "end"],
-]);
 /**
  * The Network layer (IP) link between two routers. Supports sending and receiving network layer IP Messages.
  */
@@ -97,140 +83,37 @@ export class IPLinkInterface {
    * @param message
    * @param color Color of the packet to be drawn
    */
-  sendMessage = (
+  sendMessage = async (
     from: Router,
     to: IPv4Address,
     protocol: IPProtocolNumber,
     message: IPacket,
     color: string = Colors.accent,
-    animTime: number = 2500
+    duration: number = 2500
   ) => {
     const fromIpStr = this.routers.getKey(from);
-    const dest = this.getOppositeRouter(from);
     if (!fromIpStr) {
       throw new Error(
         "Unexpected sendMessage call on Link Interface. Does not connect the said IP address."
       );
     }
-    const start = getEndpointCoords.call(this, from.location);
-    const end = getEndpointCoords.call(this, dest.location);
-    const { cp1, cp2 } = getCheckpointCoords.call(
-      this,
-      from.location,
-      dest.location
-    );
-    this.animatePacket(
-      start,
-      end,
-      cp1,
-      cp2,
-      start,
-      Date.now(),
-      Date.now() + animTime,
-      color,
-      "start"
-    );
     const fromIp = IPv4Address.fromString(fromIpStr);
     switch (protocol) {
       case IPProtocolNumber.ospf:
-        return ospfMessageHandler(this.id, fromIp, to, message, this.routers);
+        ospfMessageHandler.call(
+          this,
+          this.id,
+          fromIp,
+          to,
+          message,
+          this.routers,
+          color,
+          duration
+        );
+        break;
       default:
         break;
     }
-  };
-
-  animatePacket = (
-    from: Point2D,
-    to: Point2D,
-    cp1: Point2D,
-    cp2: Point2D,
-    prevPosition: Point2D,
-    startTime: number,
-    endTime: number,
-    color: string,
-    status: "start" | "cp1" | "cp2" | "end",
-    cpStartTime: number = startTime
-  ) => {
-    const rectW = 16,
-      rectH = 10;
-    if (!this.elementLayerContext) {
-      return;
-    }
-    const { distance: totalDistance } = getSlopeAngleDist2D(from, to);
-    const totalTime = endTime - startTime;
-    const context = this.elementLayerContext;
-    const { p1: low } = getAllRectPointsFromCentroid(
-      prevPosition,
-      rectW,
-      rectH
-    );
-    context.clearRect(low[0] - 1, low[1] - 1, rectW + 2, rectH + 2);
-    let p1: Point2D, p2: Point2D;
-    switch (status) {
-      case "start":
-        p1 = from;
-        p2 = cp1;
-        break;
-      case "cp1":
-        p1 = cp1;
-        p2 = cp2;
-        break;
-      case "cp2":
-      default:
-        p1 = cp2;
-        p2 = to;
-        break;
-    }
-    const { distance } = getSlopeAngleDist2D(p1, p2);
-    const time = (distance * totalTime) / totalDistance;
-    const position = drawPacket.call(
-      this,
-      p1,
-      p2,
-      cpStartTime,
-      time,
-      { rectW, rectH },
-      color
-    );
-    const [aX, aY] = p1;
-    const [bX, bY] = p2;
-    const [px, py] = prevPosition;
-    const [cp1x, cp1y] = cp1;
-    const [cp2x, cp2y] = cp2;
-    const cpx = status === "start" ? cp1x : status === "cp1" ? cp2x : bX;
-    const cpy = status === "start" ? cp1y : status === "cp1" ? cp2y : bY;
-    const directionX = bX === aX ? "none" : bX < aX ? "left" : "right";
-    const directionY = bY === aY ? "none" : bY < aY ? "top" : "bottom";
-    const isPastCp =
-      directionX === "none"
-        ? directionY === "top"
-          ? py <= cpy
-          : py >= cpy
-        : directionX === "right"
-        ? px >= cpx
-        : px <= cpx;
-    if (isPastCp) {
-      status = nextPacketStatus.get(status) || "start";
-      cpStartTime = Date.now();
-    }
-    if (status === "end") {
-      context.clearRect(position[0] - 1, position[1] - 1, rectW + 2, rectH + 2);
-      return;
-    }
-    window.requestAnimationFrame(() =>
-      this.animatePacket(
-        from,
-        to,
-        cp1,
-        cp2,
-        position,
-        startTime,
-        endTime,
-        color,
-        status,
-        cpStartTime
-      )
-    );
   };
 
   draw = (routerA: Router, routerB: Router) => {
--- src/types/geometry.ts
+++ src/types/geometry.ts
@@ -2,3 +2,5 @@
  * Defines a point on the grid. `[<x_coordinate>, <y_coordinate>]`
  */
 export type Point2D = [number, number];
+
+export type RectDim = { w: number; h: number };
