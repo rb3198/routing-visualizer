049522831212949bb3063ffba671295cb674bb1e
rb3198 <ronitbhatia@hotmail.com>
2024-09-12 19:04:50 -0400

    1: Document Types of LSAs

--- src/entities/ospf/enum/ls_type.ts
+++ src/entities/ospf/enum/ls_type.ts
@@ -1,7 +1,32 @@
 export enum LSType {
+  /**
+   * | Originated By | Describes | Flooded Through |
+   * |:--------------|:----------|:----------------|
+   * | All Routers| Collected States of the router's interfaces | Single Area |
+   */
   RouterLSA = 1,
+  /**
+   * | Originated By | Describes | Flooded Through |
+   * |:--------------|:----------|:----------------|
+   * | Designated Router | List of routers in the network | Single Area |
+   */
   NetworkLSA,
+  /**
+   * | Originated By | Describes | Flooded Through |
+   * |:--------------|:----------|:----------------|
+   * | Area Border Routers | A route to a destination network outside the area | Single Area |
+   */
   SummaryIpLSA,
-  SummaryAsrLSA,
+  /**
+   * | Originated By | Describes | Flooded Through |
+   * |:--------------|:----------|:----------------|
+   * | Area Border Routers | A route to a destination Area Boundary Router (ABR) outside the area | Single Area |
+   */
+  SummaryAsBrLSA,
+  /**
+   * | Originated By | Describes | Flooded Through |
+   * |:--------------|:----------|:----------------|
+   * | AS Boundary Routers | A route to a destination outside the AS | Throughout the AS |
+   */
   ASExternalLSA,
 }
