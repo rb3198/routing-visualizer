69f0da9e243000940168ae74695a11f6088c916d
rb3198 <ronitbhatia@hotmail.com>
2024-09-12 18:57:20 -0400

    3: Add support for comparing IP Addresses

--- src/entities/ip/ipv4_address.ts
+++ src/entities/ip/ipv4_address.ts
@@ -38,4 +38,43 @@ export class IPv4Address {
     this.toString() === comparedIp.toString();
 
   referenceEquals = (comparedIp: IPv4Address) => this === comparedIp;
+
+  /**
+   * Method to compare with another IP Address.
+   *
+   * Comparison is made from the MSB onward (Left to Right).
+   * @param comparedIp The other IP Address
+   * @returns
+   * - **0** if the IPs are equal.
+   * - **1** if the Current IP is > the compared IP
+   * - **-1** if the Current IP < the compared IP
+   */
+  compare = (comparedIp: IPv4Address) => {
+    if (this.equals(comparedIp)) {
+      return 0;
+    }
+    const { bytes: comparedBytes } = comparedIp;
+    for (let i = 0; i < this.bytes.length; i++) {
+      const byte = this.bytes[i];
+      const comparedByte = comparedBytes[i];
+      const isByteUndefined = typeof byte === "undefined";
+      const isComparedByteUndefined = typeof comparedByte === "undefined";
+      if (isByteUndefined && isComparedByteUndefined) {
+        return 0;
+      }
+      if (!isByteUndefined && isComparedByteUndefined) {
+        return 1;
+      }
+      if (isByteUndefined && !isComparedByteUndefined) {
+        return -1;
+      }
+      if (byte! > comparedByte!) {
+        return 1;
+      }
+      if (byte! < comparedByte!) {
+        return -1;
+      }
+    }
+    return 0;
+  };
 }
