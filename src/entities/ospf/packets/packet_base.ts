7a33519323621ab331eed3b01aba11bde7f8225e
rb3198 <ronitbhatia@hotmail.com>
2024-09-19 15:21:03 -0400

    9: Define LS Update Packets, color LSR & LSU packets

--- src/entities/ospf/packets/packet_base.ts
+++ src/entities/ospf/packets/packet_base.ts
@@ -3,10 +3,15 @@ import { DDPacketBody } from "./dd";
 import { OSPFHeader } from "./header";
 import { HelloPacketBody } from "./hello";
 import { LSRequest } from "./ls_request";
+import { LSUpdatePacketBody } from "./ls_update";
 
 export abstract class OSPFPacket implements IPacket {
   header: OSPFHeader;
-  abstract body: HelloPacketBody | DDPacketBody | LSRequest[]; // Add packet types here as you create
+  abstract body:
+    | HelloPacketBody
+    | DDPacketBody
+    | LSRequest[]
+    | LSUpdatePacketBody; // Add packet types here as you create
   constructor(header: OSPFHeader) {
     this.header = header;
   }
