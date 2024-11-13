dd9b533a518de8eaac1c7ca2b59e3104e727b0d2
rb3198 <ronitbhatia@hotmail.com>
2024-11-13 00:02:16 -0500

    12: Routing Table Row structure overhaul - Obeys OSPF routing table structure more faithfully

--- src/entities/ospf/table_rows/routing_table_row.ts
+++ src/entities/ospf/table_rows/routing_table_row.ts
@@ -1,25 +1,59 @@
 import { Router } from "../../router";
 import { IPv4Address } from "../../ip/ipv4_address";
+import { LSA } from "../lsa";
+import { NextHop } from "../shortest_path_tree/transit_vertex_data";
+
+export type Destination = "router" | "network";
 
 export class RoutingTableRow {
-  destination: IPv4Address;
-  nextHop: IPv4Address;
-  router: Router;
+  destType: Destination;
+  /**
+   * For networks, the identifier is their associated IP address.  For routers, the identifier is the OSPF
+   * Router ID.
+   */
+  destinationId: IPv4Address;
+  /**
+   * Only defined for networks. Helps define the network's IP address range together with the `destinationId`.
+   */
+  addressMask?: IPv4Address;
+  /**
+   * This field indicates the area whose link state information has led to the routing table entry's collection of paths.
+   * This is called the entry's associated area.  For sets of AS external paths, this field is not defined.
+   */
+  area: number;
+  pathType: "intra-area" | "inter-area" | "type1-external" | "type2-external";
   cost: number;
-  interface: string;
+  type2cost?: number;
+  linkStateOrigin: LSA | null;
+  nextHops: NextHop[];
+  /**
+   * Valid only for inter-area and AS external paths.  This field indicates the Router ID of
+   * the router advertising the summary-LSA or AS-external-LSA that led to this path.
+   */
+  advRouter?: Router;
 
-  constructor(
-    destination: IPv4Address,
-    nextHop: IPv4Address,
-    router: Router,
-    cost: number,
-    _interface: string
-  ) {
-    this.destination = destination;
-    this.nextHop = nextHop;
-    this.router = router;
+  constructor({
+    destType,
+    destinationId,
+    addressMask,
+    area,
+    pathType,
+    cost,
+    type2cost,
+    linkStateOrigin,
+    nextHops,
+    advRouter,
+  }: RoutingTableRow) {
+    this.destType = destType;
+    this.destinationId = destinationId;
+    this.addressMask = addressMask;
+    this.area = area;
+    this.pathType = pathType;
     this.cost = cost;
-    this.interface = _interface;
+    this.type2cost = type2cost;
+    this.linkStateOrigin = linkStateOrigin;
+    this.nextHops = nextHops;
+    this.advRouter = advRouter;
   }
 }
 
