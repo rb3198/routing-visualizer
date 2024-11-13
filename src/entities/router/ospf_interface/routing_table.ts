9956a5decce27f188ff64cc7da9c22d6a9fb1834
rb3198 <ronitbhatia@hotmail.com>
2024-11-13 00:08:11 -0500

    15: Store routing table calculation result in a map per area

--- src/entities/router/ospf_interface/routing_table.ts
+++ src/entities/router/ospf_interface/routing_table.ts
@@ -1,46 +1,87 @@
-import {
-  PriorityQueueFactory,
-  PriorityQueue,
-} from "ts-data-structures-collection/queues";
 import { OSPFInterface } from ".";
 import { routingTableCalcWorkerPool } from "src/worker_pools/ospf/routing_table_calculation_pool";
 import { RoutingTable } from "src/entities/ospf/table_rows/routing_table_row";
+import { RoutingTableCalculationResult } from "src/types/ospf/routing_table_calc_result";
+import { IPv4Address } from "src/entities/ip/ipv4_address";
 
 export class RoutingTableManager {
   ospfInterface: OSPFInterface;
-  prevTable: RoutingTable;
-  table: RoutingTable;
-  candidates: PriorityQueue<number>;
-  calculating: boolean;
-  calculationScheduled: boolean;
+  /**
+   * Map of previous routing tables for each of the areas that the router belongs to.
+   */
+  prevTableMap: Record<number, RoutingTable>;
+  /**
+   * Map of current routing tables for each of the areas that the router belongs to.
+   */
+  tableMap: Record<number, RoutingTable>;
+  /**
+   * Map of calculation status for each area that the router belongs to.
+   */
+  calculatingMap: Record<number, boolean>;
+  /**
+   * Map of scheduling status for the calculation of the table for each of the areas that the router belongs to.
+   */
+  calculationScheduledMap: Record<number, boolean>;
   constructor(ospfInterface: OSPFInterface) {
     this.ospfInterface = ospfInterface;
-    this.prevTable = [];
-    this.table = [];
-    this.candidates = PriorityQueueFactory();
-    this.calculating = false;
-    this.calculationScheduled = false;
+    this.prevTableMap = [];
+    this.tableMap = [];
+    this.calculatingMap = {};
+    this.calculationScheduledMap = {};
   }
 
-  onCalculation = (e: MessageEvent) => {
-    console.log("On calculation invoked");
-    console.log(e.data);
-    this.calculating = false;
-    if (this.calculationScheduled) {
-      this.calculationScheduled = false;
-      this.calculate();
+  onCalculation = (e: MessageEvent<RoutingTableCalculationResult>) => {
+    const { router, lsDb } = this.ospfInterface;
+    const { ipInterfaces } = router;
+    const { areaId, table } = e.data;
+    for (const entry of table) {
+      const { destinationId, destType } = entry;
+      if (destType !== "network") {
+        continue;
+      }
+      entry.nextHops = entry.nextHops.map((nextHop) => {
+        if (nextHop.interfaceId.includes("255.255")) {
+          for (let [ipInterfaceId, ipInterface] of ipInterfaces.entries()) {
+            const oppositeRouter =
+              ipInterface.ipInterface.getOppositeRouter(router);
+            const oppositeRouterIp =
+              ipInterface.ipInterface.getSelfIpAddress(oppositeRouter);
+            if (
+              oppositeRouterIp &&
+              IPv4Address.fromString(oppositeRouterIp).fromSameSubnet(
+                new IPv4Address(...destinationId.bytes)
+              )
+            ) {
+              return {
+                ...nextHop,
+                interfaceId: ipInterfaceId,
+              };
+            }
+          }
+        }
+        return nextHop;
+      });
+    }
+    this.tableMap[areaId] = table;
+    const isAreaBorderRouter = Object.keys(lsDb.db).length > 1;
+    if (isAreaBorderRouter) {
+      // generate summary LSAs for the area
+    }
+    this.calculatingMap[areaId] = false;
+    if (this.calculationScheduledMap[areaId]) {
+      this.calculationScheduledMap[areaId] = false;
+      this.calculate(areaId);
     }
   };
 
-  calculate = () => {
-    if (this.calculating) {
-      this.calculationScheduled = true;
+  calculate = (areaId: number) => {
+    if (this.calculatingMap[areaId]) {
+      this.calculationScheduledMap[areaId] = true;
       return;
     }
-    this.calculating = true;
-    routingTableCalcWorkerPool.calculate(this);
+    this.calculatingMap[areaId] = true;
     // // Step 1: Invalidate the current routing table.
-    // this.prevTable = [...this.table];
-    // this.table = [];
+    this.prevTableMap[areaId] = [...(this.tableMap[areaId] ?? [])];
+    routingTableCalcWorkerPool.calculate(this, areaId);
   };
 }
