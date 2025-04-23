6ed855210c7f6a810ce2fd0a007958c5656c3858
rb3198 <ronitbhatia@hotmail.com>
2025-04-23 13:25:53 -0500

    8: Implement Infinite canvas - Add / remove rows / columns dynamically on pan

--- src/components/grid_manager/index.tsx
+++ src/components/grid_manager/index.tsx
@@ -1,13 +1,27 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useLayoutEffect, useRef, useState } from "react";
 import { Grid } from "../grid";
 import { GridCell } from "../../entities/geometry/grid_cell";
 import { AreaManager } from "../area_manager";
 import { Point2D } from "src/types/geometry";
 import { addVectors, subtractVectors } from "src/utils/geometry";
 import { MouseButton, MouseRightEventHandler } from "src/types/common/mouse";
+import { DEFAULT_AREA_SIZE } from "src/constants/sizing";
 
 const MIN_ZOOM = 0.5;
 const MAX_ZOOM = 5;
+
+const getVisibleWorldBounds = (
+  canvasWidth: number,
+  canvasHeight: number,
+  offset: Point2D
+) => {
+  const [x, y] = offset;
+  const startX = -x;
+  const startY = -y;
+  const endX = startX + canvasWidth;
+  const endY = startY + canvasHeight;
+  return { startX, endX, startY, endY };
+};
 
 export type Drag = {
   start: Point2D;
@@ -17,12 +31,80 @@ export type Drag = {
 };
 
 export const GridManager: React.FC = () => {
-  const [gridSize, setGridSize] = useState(30);
+  const [cellSize, setCellSize] = useState(30);
   const [grid, setGrid] = useState<GridCell[][]>([]);
   const zoomRef = useRef(1);
   const canvasOffsetRef = useRef<Point2D>([0, 0]);
   const dragRef = useRef<Drag>();
   const gridCanvasRef = useRef<HTMLCanvasElement>(null);
+
+  useLayoutEffect(() => {
+    const { clientWidth } = document.documentElement;
+    let cellSize = 48; // Large desktops
+    if (clientWidth < 800) cellSize = 32; // Tablets and small screens
+    if (clientWidth < 1400) cellSize = 40; // Standard desktops
+    setCellSize(cellSize);
+    window.cellSize = cellSize;
+  }, []);
+
+  const expandGrid = useCallback(() => {
+    const canvas = gridCanvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    const offset = canvasOffsetRef.current;
+    const { width, height } = canvas.getBoundingClientRect();
+    const { startX, startY, endX, endY } = getVisibleWorldBounds(
+      width,
+      height,
+      offset
+    );
+    if (!grid.length || !grid[0].length) {
+      return;
+    }
+    const gridMinX = grid[0][0].x;
+    const gridMaxX = grid[0][grid[0].length - 1].x + cellSize;
+    const gridMinY = grid[0][0].y;
+    const gridMaxY = grid[grid.length - 1][0].y + cellSize;
+    const newGrid = [...grid];
+    if (startX < gridMinX) {
+      // Add column to the left
+      newGrid.forEach((row) =>
+        row.unshift(new GridCell(row[0].x - cellSize, row[0].y, cellSize))
+      );
+    }
+    if (gridMaxX < endX) {
+      // Add column to the right
+      newGrid.forEach((row) =>
+        row.push(
+          new GridCell(row[row.length - 1].x + cellSize, row[0].y, cellSize)
+        )
+      );
+    }
+    const firstRow = newGrid[0];
+    if (gridMaxY < endY) {
+      // Add row to the bottom
+      newGrid.push(
+        new Array(firstRow.length)
+          .fill("")
+          .map((_, idx) => new GridCell(firstRow[idx].x, gridMaxY, cellSize))
+      );
+    }
+    if (startY < gridMinY) {
+      newGrid.unshift(
+        new Array(firstRow.length)
+          .fill("")
+          .map(
+            (_, idx) =>
+              new GridCell(firstRow[idx].x, firstRow[0].y - cellSize, cellSize)
+          )
+      );
+    }
+    setGrid(newGrid);
+    return (
+      newGrid.length !== grid.length || newGrid[0].length !== grid[0].length
+    );
+  }, [cellSize, grid]);
 
   const drawGrid = useCallback(() => {
     const canvas = gridCanvasRef.current;
@@ -32,12 +114,13 @@ export const GridManager: React.FC = () => {
     }
     const { width, height } = canvas.getBoundingClientRect();
     ctx.clearRect(0, 0, width, height);
+    expandGrid();
     for (const row of grid) {
       for (const cell of row) {
         cell.drawEmpty(ctx);
       }
     }
-  }, [grid]);
+  }, [grid, expandGrid]);
 
   //#region Ref setters
   const setZoom = useCallback(
@@ -144,12 +227,12 @@ export const GridManager: React.FC = () => {
     <>
       <Grid
         setGrid={setGrid}
-        gridSize={gridSize}
+        cellSize={cellSize}
         gridCanvasRef={gridCanvasRef}
       />
       <AreaManager
         gridRect={grid}
-        defaultAreaSize={Math.ceil(gridSize / 5)}
+        defaultAreaSize={DEFAULT_AREA_SIZE}
         zoomHandler={zoomHandler}
         onMouseRightDown={onMouseRightDown}
         onMouseRightMove={onMouseRightMove}
